0d1691aba28d6e9dcfbcc24b13dc2fd675d94445
Vladislav <vlados.billi@gmail.com>
2024-10-16 16:28:04 +0300

    Сделал header

--- front/src/components/AppHeader/AppHeader.jsx
+++ front/src/components/AppHeader/AppHeader.jsx
@@ -4,43 +4,18 @@ const AppHeader = () => {
     return (
         <header>
             <nav className={AppHeaderStyles.nav}>
-                <p>Сет
-                    Рюмочных
-                </p>
+                <img src="/logo.svg" alt="Логотип" />
+                <div className={AppHeaderStyles.title}>
+                    <p>Сет</p>
+                    <p>Рюмочных</p>
+                </div>
+
                 <p>Новости/Ивенты</p>
                 <p>Меню</p>
                 <p>Рюмочные</p>
                 <p>Контакты</p>
                 <p>О проекте</p>
             </nav>
-            {/*<nav className={AppHeaderStyles.nav}>*/}
-            {/*    <div className={`p-1 ${AppHeaderStyles.rowColumn}`}>*/}
-            {/*        <BurgerIcon type="secondary"/>*/}
-            {/*        <NavLink to='/'*/}
-            {/*                 className={({isActive}) => `pl-3 mr-10 text text_type_main-default ${isActive ? '' : 'text_color_inactive'} ${AppHeaderStyles.link}`}>*/}
-            {/*            Конструктор*/}
-            {/*        </NavLink>*/}
-
-            {/*        <ListIcon type="secondary"/>*/}
-
-            {/*        <NavLink to='/profile/orders'*/}
-            {/*                 className={({isActive}) => `pl-3 text text_type_main-default ${isActive ? '' : 'text_color_inactive'} ${AppHeaderStyles.link}`}>*/}
-            {/*            Лента заказов*/}
-            {/*        </NavLink>*/}
-            {/*    </div>*/}
-            {/*    <div className="p-1">*/}
-            {/*        <Logo/>*/}
-            {/*    </div>*/}
-            {/*    <div className={`p-1 ${AppHeaderStyles.rowColumn}`}>*/}
-
-            {/*        <ProfileIcon type="secondary"/>*/}
-
-            {/*        <NavLink to='/profile'*/}
-            {/*                 className={({isActive}) => `pl-3 text text_type_main-default ${isActive ? '' : 'text_color_inactive'} ${AppHeaderStyles.link}`}>*/}
-            {/*            Личный кабинет*/}
-            {/*        </NavLink>*/}
-            {/*    </div>*/}
-            {/*</nav>*/}
         </header>
     );
 };
